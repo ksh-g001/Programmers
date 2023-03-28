6c9225bd4e0817051ed547dc9d7adaa9719b3c69
ksh-g001 <114320282+ksh-g001@users.noreply.github.com>
2023-03-28 13:38:42 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 13.61 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int length = s.length();
+        
+        if(length % 2 ==0)
+		{
+			answer+=s.charAt(length/2-1);
+			answer+=s.charAt(length/2);
+		}
+		
+		else
+		{
+			answer+=s.charAt((int)length/2);
+		}
+        
+        return answer;
+    }
+}
