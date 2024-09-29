cfc0443d663ff348a6e38ff1b75b99c31b280736
kim suhyun <114320282+ksh-g001@users.noreply.github.com>
2024-09-29 23:54:28 +0900

    [level 2] Title: n^2 배열 자르기, Time: 45.53 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.kt
@@ -0,0 +1,18 @@
+import kotlin.math.max
+
+class Solution {
+    fun solution(n: Int, left: Long, right: Long): IntArray {
+        var answer: IntArray = intArrayOf()
+        val array = mutableListOf<Int>()
+
+        for (cnt in  left..right){
+            val quotient = (cnt)/n
+            val remainder = (cnt)%n
+            val e = max(quotient, remainder)
+            array.add(e.toInt()+1)
+        }
+
+        answer = answer.plus(array)
+        return answer
+    }
+}
